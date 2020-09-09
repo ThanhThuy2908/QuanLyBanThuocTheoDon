a1c4bf40de1c06a19d231eda388c78b214ad986a
ThanhThuy2908 <Thanhthuybt2908@gmail.com>
2020-09-09 23:16:46 +0700

    Tao Form thong ke thuoc ban
    Chi moi tao form thong ke thanh tich ban thuoc

---
+++ Quan Ly Ban Thuoc/QuanLyPhongKhamNhi/view/FormTkThuoc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace QuanLyPhongKhamNhi.view
+{
+    public partial class FormTkThuoc : DevExpress.XtraReports.UI.XtraReport
+    {
+        public FormTkThuoc()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
